37fea57fd3fee957cb54ce2560284c50f92cee32
Isaque Menezes <isaque-menezes@hotmail.com>
2022-08-01 16:55:08 -0300

    criando uma lista

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Button from './components/Button';
 import Form from './components/Form';
+import Lista from './components/List';
 
 
 function App() {
   return (
     <div className="App">
       <Form />
+      <Lista />
     </div>
   );
 }
